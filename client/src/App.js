4b6ce2c3657176f24a6fd41b57450eebcf379e4c
koedbymonkey <tsnavely4@gmail.com>
2020-08-28 13:11:04 -0700

    Added Private Route for Home page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
@@ -27,7 +28,7 @@ const App = () => {
 							<div className='container text-center'>
 								<Alerts />
 								<Switch>
-									<Route exact path='/' component={Home} />
+									<PrivateRoute exact path='/' component={Home} />
 									<Route exact path='/about' component={About} />
 									<Route exact path='/register' component={Register} />
 									<Route exact path='/login' component={Login} />
