d1d98d8eb563d3b446aa773fdbae0ee3cf887294
koedbymonkey <tsnavely4@gmail.com>
2020-08-28 11:18:27 -0700

    Changed alert page layout

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
 	return (
-		<div className='container bg-secondary m-3 p-2'>
+		<div className='card m-3 p-2'>
 			<h2>About This App</h2>
 			<p className='my-1'>
 				This is a full stack React app for keeping contacts
