ded79d05ec62eb91ec708d8cbd375f557917a4e4
koedbymonkey <tsnavely4@gmail.com>
2020-08-28 10:21:41 -0700

    Added Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -8,19 +9,21 @@ import './App.css';
 
 const App = () => {
 	return (
-		<ContactState>
-			<Router>
-				<Fragment>
-					<Navbar />
-					<div className='container text-center'>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/about' component={About} />
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className='container text-center'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
 	);
 };
 
